d1d69e661403fa45e82b96e718dd440f1d07b298
XiaoyuOfc <111475019+XiaoyuOfc@users.noreply.github.com>
2022-09-04 18:04:46 +0700

    Create _jasrun.js

---
+++ plugins/_jasrun.js
@@ -0,0 +1,29 @@
+let handler = async (m, { conn, command, args }) => {
+let tetekss = `───〔 *Minat?* 〕───
+
+${pickRandom(global.ngawur)}`
+conn.sendBut(m.chat, tetekss, wm3, 'Owner', '.owner', m) 
+}
+handler.help = ['jasrun']
+handler.tags = ['quotes']
+handler.command = /^(jasrun)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+global.ngawur = [
+"Open Jasa Run Heroku\nHARGA\n10K SEBULAN\n15 PERMANEN\nSYARATNYA: Pakai Sc Yang Saya Gunakan, Karena Sc Yang Saya Gunakan Work heroku (ON 24 JAM DI HEROKU)\nJika Minat Hubungi Saja http://wa.me/+66615143033",
+]
