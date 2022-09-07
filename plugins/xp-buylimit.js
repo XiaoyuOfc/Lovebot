ba2327e9a158656a659c0b183e880c35de637250
XiaoyuOfc <111475019+XiaoyuOfc@users.noreply.github.com>
2022-09-07 08:26:51 +0700

    Update xp-buylimit.js

--- plugins/xp-buylimit.js
+++ plugins/xp-buylimit.js
@@ -1,34 +1,20 @@
 const xpperlimit = 350 
 let handler = async (m, { conn, command, args }) => {
-  let ftroli = {
-    key : {
-    remoteJid: '6283136505591-1614953337@g.us',
-    participant : '0@s.whatsapp.net'
-    },
-    message: {
-    orderMessage: {
-    itemCount : 2022,
-    status: 1,
-    surface : 1,
-    message: 'Get Limit', 
-    orderTitle: `Wabot`,
-    thumbnail: 'https://telegra.ph/file/5ecbec3e82e247671a18e.jpg', 
-    sellerJid: '0@s.whatsapp.net' 
-    }
-    }
-    }
   let count = command.replace(/^buy/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
   if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
     global.db.data.users[m.sender].exp -= xpperlimit * count
     global.db.data.users[m.sender].limit += count
-    conn.reply(m.chat, `Berhasil Membeli ${count} Limit 🏷 Dengan ${xpperlimit * count} Xp 🧬`, ftroli) 
-  } else conn.reply(m.chat, `XP anda tidak mencukupi untuk membeli ${count} limit, Anda dapat menambah exp dengan bermain game dan rpg`, ftroli) 
+    conn.sendBut(m.chat, `-${xpperlimit * count} XP\n+ ${count} Limit`, wm, 'Buy All', '.buyall', m)
+  } else conn.sendBut(m.chat, `Limit Anda Hanya Bisa Di Beli Dengan Cheat Game!! \n\n Abaikan Pesan Ini Bila Ingin Bermain Bersih Tanpa Curang.`, wm4, 'Setuju Cheat', '.cheat', m)
+  m.reply('```Bot Tidak Bisa Menambah Limit User, Namun Bot Bisa Memasang Cheat Limit Dan Cheat All Game```\n\n「 *Hanya User Premium!* 」')
+  let footer= 'Silahkan Pilih Salah Satu Opsi Di Bawah Ini'
+conn.send2But(m.chat, wm4, footer, 'Daily', '.daily', 'Weekly', '.weekly  ', m,)
 }
 handler.help = ['buy<jumlah limit>', 'buy <jumlah limit>', 'buyall']
 handler.tags = ['xp']
-handler.command = /^buylimit([0-9]+)|buylimit|buyalllimit$/i
+handler.command = /^buy([0-9]+)|buy|buyall$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
