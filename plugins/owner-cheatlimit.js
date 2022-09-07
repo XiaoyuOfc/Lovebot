9fe6313c854d5d7737889be726696049df08c3d5
XiaoyuOfc <111475019+XiaoyuOfc@users.noreply.github.com>
2022-09-07 08:38:14 +0700

    Update owner-cheatlimit.js

--- plugins/owner-cheatlimit.js
+++ plugins/owner-cheatlimit.js
@@ -3,7 +3,7 @@ let handler = async (m, { text }) => {
 m.reply('=> global.db.data.users[m.sender].limit = Infinity')
 
 }
-handler.help = ['cheatlimit']
+handler.help = ['cheatliimit']
 handler.tags = ['owner']
 handler.command = /^cheatlimit$/i
 
