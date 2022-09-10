1ecadd766feeb256ac094801c9b695a31b3d89ca
XiaoyuOfc <111475019+XiaoyuOfc@users.noreply.github.com>
2022-09-10 12:04:06 +0700

    Update main-bothi.js

--- plugins/main-bothi.js
+++ plugins/main-bothi.js
@@ -2,11 +2,10 @@
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://database.tioclkp02.repl.co/IMG-20220724-WA0225.png', m, { packname: "database replit", author: "@tioclkp.replit" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/8427053ee47e607a3819b.jpg', m, { packname: "Ngen", author: "Masal" })
 }
 
 handler.customPrefix = /^(Bot|p)$/i
 handler.command = new RegExp
 
 module.exports = handler
-
