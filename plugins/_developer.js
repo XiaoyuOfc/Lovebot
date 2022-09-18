26b66d611079f27f9b722ec5fcf41e008b3763ba
XiaoyuOfc <111475019+XiaoyuOfc@users.noreply.github.com>
2022-09-19 06:58:40 +0700

    Update _developer.js

--- plugins/_developer.js
+++ plugins/_developer.js
@@ -14,7 +14,7 @@ VERSION:3.0
 N:Sy;Bot;;;
 FN: Ordinary people 
 item.ORG: Creator Bot
-item1.TEL;waid=18254257910:18254257910@s.whatsapp.net
+item1.TEL;waid=6289652963317:6289652963317@s.whatsapp.net
 item1.X-ABLabel:Nomor Creator Bot / Owner Bot
 item2.EMAIL;type=INTERNET:Kalo Chat Gak Penting Gak Akan Di Balas.
 item2.X-ABLabel:Email
