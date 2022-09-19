d8e79c516f20dc5b5d81833803629313bdbcf188
XiaoyuOfc <111475019+XiaoyuOfc@users.noreply.github.com>
2022-09-19 07:06:51 +0700

    Update _jasrun.js

--- plugins/_jasrun.js
+++ plugins/_jasrun.js
@@ -25,5 +25,5 @@ function pickRandom(list) {
 }
 
 global.jasrun = [
-"*Suntik Followers Instagram*\n\ninstagram follower bule\n•500 follower | Rp3.500\n•1000 follower | Rp6.500\n•2000 follower | Rp11.500\n•5000 follower | Rp30.000\nInstagram follower Indonesia Real aktif\n•500 follower |  Rp25.000\n•1000 follower | Rp50.000\n•5000 follower | Rp240.000\n\n*Ada juga Instagram like, tiktok followers, tiktok like, Dll*\nbutuh penawaran lain nya? bisa bisik bisik lewat wa\n\norder?\nlangsung chat saya\nwa.me/66615143033\npembayaran all payment",
+"*Suntik Followers Instagram*\n\ninstagram follower bule\n•500 follower | Rp3.500\n•1000 follower | Rp6.500\n•2000 follower | Rp11.500\n•5000 follower | Rp30.000\nInstagram follower Indonesia Real aktif\n•500 follower |  Rp25.000\n•1000 follower | Rp50.000\n•5000 follower | Rp240.000\n\n*Ada juga Instagram like, tiktok followers, tiktok like, Dll*\nbutuh penawaran lain nya? bisa bisik bisik lewat wa\n\norder?\nlangsung chat saya\nwa.me/+6289652963317\npembayaran all payment",
 ] 
