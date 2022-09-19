9135a18ce94092fd5c221ee22a38ec3d44866561
XiaoyuOfc <111475019+XiaoyuOfc@users.noreply.github.com>
2022-09-19 18:34:20 +0700

    Update _nokos.js

--- plugins/_nokos.js
+++ plugins/_nokos.js
@@ -25,5 +25,5 @@ function pickRandom(list) {
 }
 
 global.nokos = [
-"Ready nokos WhatsApp\nHarga:\nIndo +62 4k\nRussia+7 3k\nThailand +66 6k\n*Seperti saya +66*\nChina +86 7k\n*Dan lain lain*\nMinat? Hubungi wa.me/+66615143033\n\nApa itu nokos?\n*Nokos adalah Nomor kosong yang dijual untuk di verifikasi ke WhatsApp*\nJika anda membeli nokos di saya,\nSaya akan kasih nomor, dan anda harus mendaftarkan nomornya ke WhatsApp,\n*Nanti saya kasih codenya*\n\nPromo ,USA\n*Contoh Nomor: +1 (825) 888-0882*\nCuman 5k, *Stok Terbatas!!*\n_Via pulsa dana & Qris all payment._",
+"Ready nokos WhatsApp\nHarga:\nIndo +62 4k\nRussia+7 3k\nThailand +66 6k\n*Seperti saya +66*\nChina +86 7k\n*Dan lain lain*\nMinat? Hubungi http://Wa.me/+62812-2850-3965\n\nApa itu nokos?\n*Nokos adalah Nomor kosong yang dijual untuk di verifikasi ke WhatsApp*\nJika anda membeli nokos di saya,\nSaya akan kasih nomor, dan anda harus mendaftarkan nomornya ke WhatsApp,\n*Nanti saya kasih codenya*\n\nPromo ,USA\n*Contoh Nomor: +1 (825) 888-0882*\nCuman 5k, *Stok Terbatas!!*\n_Via pulsa dana & Qris all payment._",
 ]
